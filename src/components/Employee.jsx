734e0466c24a3ec9ea49fd29bde5bf9d3d161614
Ashish Sharda <ashishjsharda@gmail.com>
2020-05-17 09:11:46 -0400

    Add all my files

---
+++ src/components/Employee.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+class Employee extends Component {
+    render() { 
+        return ( <div>
+            <h1> Employee Name is Rick</h1>
+            <h1> Employee Id is 201</h1>
+        </div> );
+    }
+}
+ 
+export default Employee;
